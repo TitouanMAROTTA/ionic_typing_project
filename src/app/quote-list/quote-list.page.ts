0e883c731478a97206a71f658cc077e6f28b08d1
TitouanMAROTTA <titouan.marotta@gmail.com>
2023-03-15 04:33:23 +0100

    update quoteList (add *ngFor to generate list with local database)

--- src/app/quote-list/quote-list.page.ts
+++ src/app/quote-list/quote-list.page.ts
@@ -27,6 +27,32 @@ import { TranslationService } from '../services/translation.service';
 })
 export class QuoteListPage implements OnInit {
   public currentLanguage: string = 'en';
+  quotes = [
+    {
+      id: 1,
+      title: 'Super quote',
+      content: 'Une citation n\'est jamais rien de plus que quelques mots.',
+      author: 'Titouan'
+    },
+    {
+      id: 2,
+      title: 'Super quote',
+      content: 'Une citation n\'est jamais rien de plus que quelques mots.',
+      author: 'Titouan'
+    },
+    {
+      id: 3,
+      title: 'Super quote',
+      content: 'Une citation n\'est jamais rien de plus que quelques mots.',
+      author: 'Titouan'
+    },
+    {
+      id: 4,
+      title: 'Super quote',
+      content: 'Une citation n\'est jamais rien de plus que quelques mots.',
+      author: 'Titouan'
+    }
+  ]
 
   constructor(public translationService: TranslationService) { }
 
